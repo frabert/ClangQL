5d44a69eed1ec3828ea408549b8c89c461ca2c5c
Francesco Bertolaccini <francesco@bertolaccini.dev>
2021-05-26 14:46:53 +0200

    Make ClangQLModule final

--- src/ClangQLModule.hpp
+++ src/ClangQLModule.hpp
@@ -2,7 +2,7 @@
 #define CLANGQLMODULE_HPP
 #include "Module.hpp"
 
-class ClangQLModule : public Module {
+class ClangQLModule final : public Module {
 public:
   virtual std::unique_ptr<VirtualTable>
   Create(sqlite3 *db, int argc, const char *const *argv) override;
